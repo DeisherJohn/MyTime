cea3020abc964ae1c154d58b830cb00c16490ead
JohnDeisher <john.deisher2013@gmail.com>
2019-11-04 13:11:19 +1100

    Added basic config file and admin pin setting

---
+++ MyTime/Scenes/Admin_creator/AdminPin.gd
@@ -0,0 +1,39 @@
+extends Control
+
+
+onready var info = $WindowDialog/MarginContainer/VBoxContainer
+
+onready var pin1 = info.get_node("LinePinOne").get_text()
+onready var pin2 = info.get_node("LinePinTwo").get_text()
+
+func show_win():
+	$WindowDialog.show()
+
+
+func _on_Button_pressed():
+	if pin1.is_valid_integer():
+		print("PIN IS GOOD")
+		settings.set_admin_pin(pin1)
+		settings.save_settings()
+		self.queue_free()
+	else:
+		print("PIN IS BAD")
+	pass # Replace with function body.
+
+
+func _on_LinePinOne_text_changed(new_text):
+	pin1 = new_text
+	pins_match()
+
+
+func _on_LinePinTwo_text_changed(new_text):
+	pin2 = new_text
+	pins_match()
+
+func pins_match():
+	if pin1 == pin2 and len(pin1) == 4 and pin1.is_valid_integer():
+		info.get_node("Label3").hide()
+		info.get_node("Button").disabled = false
+	else:
+		info.get_node("Label3").show()
+		info.get_node("Button").disabled = true
--- MyTime/Scripts/SettingsManager.gd
+++ MyTime/Scripts/SettingsManager.gd
@@ -7,18 +7,46 @@ enum DATE_FORMAT {MMDDYYYY = 0, DDMMYYYY = 1}
 var _config_file = ConfigFile.new()
 
 var _settings = {
-	"admin":{
-		"pin":null
+	"hidden":{
+		"admin_pin":"0000"
 	},
 	"date":{
 		"format":DATE_FORMAT.MMDDYYYY
 	}
-	
 }
 
+func _ready():
+	load_settings()
 
 
+func set_admin_pin(pin):
+	_settings["hidden"]["admin_pin"] = pin
 
-func load_settings():
+func get_admin_pin():
+	return _settings["hidden"]["admin_pin"]
+
+func save_settings():
+	for section in _settings.keys():
+		for key in _settings[section]:
+			_config_file.set_value(section, key, _settings[section][key])
+			
+	_config_file.save(SAVE_PATH)	
+
+func load_settings(main_window = null):
+	var error = _config_file.load(SAVE_PATH)
 	
-	pass
+	if error != OK:
+		print("FAILED TO LOAD SETTINS: ERROR CODE %s" % error)
+		return null
+	
+	for section in _settings.keys():
+		for key in _settings[section]:
+			_settings[section][key] = _config_file.get_value(section, key)
+	
+	
+	if main_window != null and get_admin_pin() == "0000":
+		print("NEED NEW ADMIN PIN")
+		var adminPinScene = load("res://Scenes/Admin_creator/AdminPin.tscn")
+		var create_pin = adminPinScene.instance()
+		main_window.add_child(create_pin)
+		create_pin.show_win()

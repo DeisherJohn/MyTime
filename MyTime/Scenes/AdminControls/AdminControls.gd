7bca991b966184488fbeb261acda4db9ed6082ee
JohnDeisher <john.deisher2013@gmail.com>
2019-11-08 10:56:13 +1100

    Added a clock
    clock shows system time, updates on a 3 second interval to keep cpu cost lower but without delay update of time to much. all action as still tied to system time, not this clock.

--- MyTime/Scenes/AdminControls/AdminControls.gd
+++ MyTime/Scenes/AdminControls/AdminControls.gd
@@ -7,6 +7,14 @@ signal gen_all_report()
 signal open_settings()
 signal view_time_log()
 
+func _ready():
+	
+	$PanelContainer/VBoxContainer/Clock/HBoxContainer/LabelCurrentTime.add_color_override("font_color", Color(0,0,0))
+	$PanelContainer/VBoxContainer/Clock/HBoxContainer/LabelTime.add_color_override("font_color", Color(0,0,0))
+	set_current_time_text()
+	pass
+	
+	
 func _on_Button_pressed():
 	emit_signal("add_new_employee")
 	pass # Replace with function body.
@@ -62,3 +70,24 @@ func _on_LineEdit2_text_entered(new_text):
 func _on_Button7_pressed():
 	$AboutWindow.show_window()
 	pass # Replace with function body.
+
+
+func _on_Timer_timeout():
+	set_current_time_text()
+	pass # Replace with function body.
+	
+func set_current_time_text():
+	var time = OS.get_time()
+	var am_pm = "AM"
+	
+	if time["hour"] > 11:
+		am_pm = "PM"
+		time["hour"] -= 12
+		
+	if time["hour"] == 0:
+		time["hour"] = 12
+		
+	var time_string = str(time["hour"]) + ":" + str(time["minute"]) + " " + am_pm
+	#print(time_string)
+	$PanelContainer/VBoxContainer/Clock/HBoxContainer/LabelTime.set_text(time_string)
+	
